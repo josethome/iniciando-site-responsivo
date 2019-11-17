786eefb89a4b1df5e224f68483513a16f8d957bf
Jose Thome <josethome.band@hotmail.com>
2019-11-17 12:01:44 -0200

    Preparação para recebimento das tarefas executadas pelo gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', gulp.series( function() {
     return gulp.src('sass/*.sass')
-    .pipe(sass()) // converter o Sass em CSS
+    // converter o Sass em CSS
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) 
     .pipe(gulp.dest('css'));
 }));
 
@@ -15,3 +16,20 @@ gulp.task('watch', gulp.series( function() {
 
 gulp.task('default', gulp.series( ['sass', 'watch'] ));
 
+
+
+// CÓDIGO FUNCIONANDO => BEM DE BOA
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+
+// gulp.task('sass', gulp.series( function() {
+//     return gulp.src('sass/*.sass')
+//     .pipe(sass()) // converter o Sass em CSS
+//     .pipe(gulp.dest('css'));
+// }));
+
+// gulp.task('watch', gulp.series( function() {
+//     gulp.watch('sass/*.sass', gulp.parallel( ['sass'] ));
+// }));
+
+// gulp.task('default', gulp.series( ['sass', 'watch'] ));
