31b57b8f0546b5adf9000c8cfa1d078d403da247
Jose Thome <josethome.band@hotmail.com>
2019-11-16 22:13:17 -0200

    Inserindo o sass no projeto

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// Abaixo com a versão Gulp 4
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', gulp.series( function() {
+    return gulp.src('sass/*.sass')
+    .pipe(sass()) // converter o Sass em CSS
+    .pipe(gulp.dest('css'));
+}));
+
+gulp.task('watch', gulp.series( function() {
+    gulp.watch('sass/*.sass', gulp.parallel( ['sass'] ));
+}));
+
+gulp.task('default', gulp.series( ['sass', 'watch'] ));
+
