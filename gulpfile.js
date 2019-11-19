b8402a1412e83e1c98e55cd77f54ddd6102d65e8
Jose Thome <josethome.band@hotmail.com>
2019-11-19 10:03:51 -0200

    Marcação das tags html

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', gulp.series( function() {
-    return gulp.src('sass/*.sass')
-    // converter o Sass em CSS
+    return gulp.src('sass/**/*.sass')
+    // converter o Sass em CSS    
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) 
     .pipe(gulp.dest('css'));
 }));
 
 gulp.task('watch', gulp.series( function() {
-    gulp.watch('sass/*.sass', gulp.parallel( ['sass'] ));
+    gulp.watch('sass/**/*.sass', gulp.parallel( ['sass'] ));
 }));
 
 gulp.task('default', gulp.series( ['sass', 'watch'] ));
-
-
-
-// CÓDIGO FUNCIONANDO => BEM DE BOA
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-
-// gulp.task('sass', gulp.series( function() {
-//     return gulp.src('sass/*.sass')
-//     .pipe(sass()) // converter o Sass em CSS
-//     .pipe(gulp.dest('css'));
-// }));
-
-// gulp.task('watch', gulp.series( function() {
-//     gulp.watch('sass/*.sass', gulp.parallel( ['sass'] ));
-// }));
-
-// gulp.task('default', gulp.series( ['sass', 'watch'] ));
